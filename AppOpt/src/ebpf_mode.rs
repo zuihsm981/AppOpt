ef402f0c776ab76d420ea897ab3a126e5243d21d
zuihsm981 <qwe123shu@gmail.com>
2026-08-30 06:57:26 +0800

    更新 ebpf_mode.rs

--- AppOpt/src/ebpf_mode.rs
+++ AppOpt/src/ebpf_mode.rs
@@ -420,7 +420,7 @@ fn affinity_apply(
     kpm_log!("KPM affinity_apply: tid={} cpus={} cpuset_dir='{}'", tid, cpus.to_range_string(), cpuset_dir);
     let dead = affinity_set(tid, cpus, cpuset_dir, &cfg.topo);
     if !dead {
-        applied_set(bpf, tid, cpus.bits[0]);
+        applied_set(bpf, tid, cpus);
         eprintln!("KPM affinity_apply: tid={} applied_set bits={:#x}", tid, cpus.bits[0]);
         kpm_log!("KPM affinity_apply: tid={} applied_set bits={:#x}", tid, cpus.bits[0]);
     }
